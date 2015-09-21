fa5934312db6975b8154b33be51a2b77ffe8d717
Aniruddh Bharadwaj <aniruddhb@users.noreply.github.com>
2015-09-21 15:32:42 -0700

    Pushed Initial File to Github
    Contains a list of all valid points on a chess board, so as to help the main knightsTour file detect whether a chess piece can go to a space on the chess board that has already been exhausted.

---
+++ validPoints.java
@@ -0,0 +1,19 @@
+import java.util.*;
+import java.io.*;
+
+public class validPoints {
+    int addX, addY;
+    
+    public validPoints(int addX, int addY){
+        this.addX = addX;
+        this.addY = addY;
+    }
+    
+    public int getX(){
+        return addX;
+    }
+    
+    public int getY(){
+        return addY;
+    }
+}
